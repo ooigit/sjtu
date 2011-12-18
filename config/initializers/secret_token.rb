3fc439fd62edc7fcad4779e8afcb3c074d1593bc
DongQS <dongqs@gmail.com>
2011-12-18 16:38:47 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sjtu::Application.config.secret_token = '194e7a299e00c60e4b7fbf519a392358c3a216c89f002013ada53bc5b8b7b25de62c65c56c2cf30b08db33f8b3dadf7e87f9c8fae9b28151ee6926bf54988f41'
